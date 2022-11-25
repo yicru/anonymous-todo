c917f88b9b5c4db7ad01cf1b7c0714cdcbdd17ed
yuichiro miyamae <yu1ro.mya@gmail.com>
2022-11-25 11:45:05 +0900

    add: BaseLayout

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
+import { BaseLayout } from '../layouts/BaseLayout'
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <BaseLayout>
+        <Component {...pageProps} />
+      </BaseLayout>
     </ChakraProvider>
   )
 }
