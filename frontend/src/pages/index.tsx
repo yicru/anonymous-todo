299d73a4641b5d6a1f5e1a04613c6752adcf982e
yuichiro miyamae <yu1ro.mya@gmail.com>
2022-12-31 19:22:38 +0900

    update: IndexPage

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,38 @@
-import { Center, Text } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  HStack,
+  Input,
+  ListItem,
+  UnorderedList,
+} from '@chakra-ui/react'
+import { FormEvent, useState } from 'react'
 
 export default function IndexPage() {
+  const [todos, setTodos] = useState<string[]>([])
+  const [value, setValue] = useState('')
+
+  const onSubmit = (event: FormEvent) => {
+    event.preventDefault()
+    setTodos([...todos, value])
+    setValue('')
+  }
+
   return (
-    <Center h={'100vh'}>
-      <Text>Hello, Anonymous ToDo</Text>
-    </Center>
+    <Box py={8} px={4}>
+      <HStack as={'form'} onSubmit={onSubmit}>
+        <Input
+          isRequired
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <Button type={'submit'}>追加</Button>
+      </HStack>
+      <UnorderedList mt={4}>
+        {todos.map((todo) => (
+          <ListItem key={todo}>{todo}</ListItem>
+        ))}
+      </UnorderedList>
+    </Box>
   )
 }
