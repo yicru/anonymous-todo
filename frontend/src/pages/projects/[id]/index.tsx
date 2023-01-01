4cde8b72ddbad4d3b51b6f5f1aeb0cd9c96ff261
yuichiro miyamae <yu1ro.mya@gmail.com>
2023-01-01 19:00:45 +0900

    update: ProjectPage シェアボタンを追加

--- frontend/src/pages/projects/[id]/index.tsx
+++ frontend/src/pages/projects/[id]/index.tsx
@@ -1,13 +1,17 @@
 import {
   Box,
+  Button,
   Divider,
   Grid,
+  HStack,
   ListItem,
   Skeleton,
   Text,
   UnorderedList,
+  useClipboard,
 } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 import { UpdateProjectNameForm } from '@src/features/project/components/UpdateProjectNameForm'
 import { AddTaskForm } from '@src/features/task/components/AddTaskForm'
@@ -16,10 +20,16 @@ import { trpc } from '@src/libs/trpc'
 export default function ProjectPage() {
   const router = useRouter()
 
+  const { hasCopied, onCopy, setValue } = useClipboard('')
+
   const { data, isLoading } = trpc.project.byId.useQuery(
     { id: String(router.query.id) },
     { enabled: !!router.query.id },
   )
+
+  useEffect(() => {
+    setValue(process.env.NEXT_PUBLIC_APP_URL + router.asPath)
+  }, [router.asPath, setValue])
 
   return (
     <Grid gap={4} h={'full'} p={4} templateRows={'auto 1px 1fr 1px auto'}>
@@ -28,9 +38,14 @@ export default function ProjectPage() {
           {data && <UpdateProjectNameForm project={data} />}
         </Skeleton>
         <Skeleton isLoaded={!isLoading} mt={1} height={'18px'}>
-          <Text fontSize={'xs'} color={'gray.400'} mt={1}>
-            ID: {data?.id}
-          </Text>
+          <HStack>
+            <Text fontSize={'xs'} color={'gray.400'} mt={1}>
+              ID: {data?.id}
+            </Text>
+            <Button variant={'outline'} size={'xs'} onClick={onCopy}>
+              {hasCopied ? 'コピーしました！' : '共有URLをコピー'}
+            </Button>
+          </HStack>
         </Skeleton>
       </Box>
 
